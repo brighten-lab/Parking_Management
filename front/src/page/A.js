6a8ee37ed3111ea65c2682253b15599b77b45a77
혜민 <97522726+heymin2@users.noreply.github.com>
2023-06-26 16:54:45 +0900

    FEAT: 상태에 따라 색 변경

--- front/src/page/A.js
+++ front/src/page/A.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 import "../style/location.css";
 
+const Car = styled.div`
+  border: 1px solid #a7a7a7;
+  width: 80px;
+  height: 150px;
+  background-color: ${(props) => (props.isParked ? "#d9d9d9" : "red")};
+`;
+
 const A = () => {
+  const [parking] = useState([
+    {
+      zone: "a",
+      is_parked: false,
+      spot_number: 1,
+    },
+    {
+      zone: "a",
+      is_parked: false,
+      spot_number: 2,
+    },
+    {
+      zone: "a",
+      is_parked: false,
+      spot_number: 3,
+    },
+    {
+      zone: "a",
+      is_parked: true,
+      spot_number: 4,
+    },
+    {
+      zone: "a",
+      is_parked: true,
+      spot_number: 5,
+    },
+    {
+      zone: "a",
+      is_parked: true,
+      spot_number: 6,
+    },
+    {
+      zone: "a",
+      is_parked: true,
+      spot_number: 7,
+    },
+  ]);
+
+  useEffect(() => {});
+
   return (
     <div className="wrapper">
       <div className="header">
@@ -9,7 +57,7 @@ const A = () => {
           <div className="header-item">주차장 현황</div>
           <div className="header-item">주차 가능 빈자리</div>
           <div className="header-item">주차 완료 자리</div>
-          <div className="header-item">총 자리</div>
+          <div className="header-item">총 주차 자리</div>
         </div>
         <div className="header-row">
           <div className="header-item">
@@ -21,15 +69,11 @@ const A = () => {
         </div>
       </div>
       <div className="body">
-        <div className="car"></div>
-        <div className="car"></div>
-        <div className="car"></div>
-        <div className="car"></div>
-        <div className="car"></div>
-        <div className="car"></div>
-        <div className="car"></div>
-        <div className="car"></div>
-        <div className="car"></div>
+        {parking.map((car, index) => (
+          <Car key={index} isParked={car.is_parked}>
+            Zone: {car.zone} Spot: {car.spot_number}
+          </Car>
+        ))}
       </div>
     </div>
   );
