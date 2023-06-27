2059903bc5f35b4e2db1a2a38cb761c764994788
혜민 <97522726+heymin2@users.noreply.github.com>
2023-06-27 13:44:31 +0900

    ADD: 주차장 위치에 따라 전체 리스트 보내주는 API

--- server/app.py
+++ server/app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import pymysql
 
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-    return 'Hello world!'
+@app.route('/list', methods=['POST'])
+def list_parking():
+    zone = request.json.get('zone') 
+    db = pymysql.connect(host='211.57.200.6', port=3306, user='root', password='willcam1190', db='park', charset='utf8')
+    cursor = db.cursor(pymysql.cursors.DictCursor)  # DictCursor를 사용하여 딕셔너리 형태로 결과를 반환
+    sql = "SELECT * FROM parking WHERE zone = %s"
+    cursor.execute(sql, zone)
+    result = cursor.fetchall()
+    db.close()
+
+    return jsonify(result)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
