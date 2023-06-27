46d543c28cf537b61eefd1a73af461d497e0e23c
heymin2 <alddlv81@gmail.com>
2023-06-27 16:00:08 +0900

    FIX: UI 수정

--- front/src/App.js
+++ front/src/App.js
@@ -1,22 +1,14 @@
 import React from "react";
 import A from "./page/A";
 import Header from "./page/Header";
-import Left from "./page/Left";
-import styled from "styled-components";
 
-const Container = styled.div`
-  display: flex;
-  height: 100vh;
-`;
+
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Container>
-        <Left />
         <A />
-      </Container>
     </div>
   );
 };
