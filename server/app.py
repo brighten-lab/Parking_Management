786b7efc410d7e8d6040e5e8573c977917c826f3
혜민 <97522726+heymin2@users.noreply.github.com>
2023-06-27 14:22:46 +0900

    FIX: CORS 오류 해결

--- server/app.py
+++ server/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 import pymysql
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/list', methods=['POST'])
 def list_parking():
