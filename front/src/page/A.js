0504f6565b80df152a1b3316db4c76f7de0be3b4
혜민 <97522726+heymin2@users.noreply.github.com>
2023-07-12 14:33:33 +0900

    Feat: url 변경

--- front/src/page/A.js
+++ front/src/page/A.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import "../style/location.css";
 import Left from "./Left";
 
-const url = 'http://127.0.0.1:5000';
+const url = "http://211.57.200.6:5000";
 
 const Car = styled.div`
   display: flex;
@@ -19,7 +19,6 @@ const Container = styled.div`
   display: flex;
   height: 80vh;
 `;
-
 
 const A = () => {
   const [avail, setAvail] = useState(0);
@@ -109,7 +108,7 @@ const A = () => {
   ]);
 
   useEffect(() => {
-    fetch(url + '/list', {
+    fetch(url + "/list", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -125,63 +124,63 @@ const A = () => {
         console.error("error: " + error);
       });
 
-      fetch(url + '/total', {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ zone: "A" }),
+    fetch(url + "/total", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ zone: "A" }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setTotal(data);
+        console.log("성공: " + data);
       })
-        .then((response) => response.json())
-        .then((data) => {
-          setTotal(data);
-          console.log("성공: " + data);
-        })
-        .catch((error) => {
-          console.error("error: " + error);
-        });
+      .catch((error) => {
+        console.error("error: " + error);
+      });
 
-        fetch(url + '/avail', {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ zone: "A" }),
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            setAvail(data);
-            console.log("성공: " + data);
-          })
-          .catch((error) => {
-            console.error("error: " + error);
-          });
-
+    fetch(url + "/avail", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ zone: "A" }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setAvail(data);
+        console.log("성공: " + data);
+      })
+      .catch((error) => {
+        console.error("error: " + error);
+      });
   }, []);
 
   return (
     <Container>
-    <Left avail={avail} total={total}/>
-    <div className="body">
-    <div className="topPart">
-        {parking.slice(0, 12).map((car, index) => (
+      <Left avail={avail} total={total} />
+      <div className="body">
+        <div className="topPart">
+          {parking.slice(0, 12).map((car, index) => (
             <Car key={index} isParked={car.is_parked}>
-                A{index+1}
+              A{index + 1}
             </Car>
-        ))}
-    </div>
-    <div style={{height:'80px',}}></div>
-    <div className="bottomPart">
-        {parking.slice(12).map((car, index) => (
+          ))}
+        </div>
+        <div style={{ height: "80px" }}></div>
+        <div className="bottomPart">
+          {parking.slice(12).map((car, index) => (
             <Car key={index + 12} isParked={car.is_parked}>
-                B{index+1}
+              B{index + 1}
             </Car>
-        ))}
-    </div>
-    <div style={{height: '80px'}}></div>
-    <div className="build"><p>제 2 시험생산공장</p></div>
-</div>
-
+          ))}
+        </div>
+        <div style={{ height: "80px" }}></div>
+        <div className="build">
+          <p>제 2 시험생산공장</p>
+        </div>
+      </div>
     </Container>
   );
 };
