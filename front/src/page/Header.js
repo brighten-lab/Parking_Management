ba277d99a6e481d4d1b5aa3363d1d6b1f57f2c08
heymin2 <alddlv81@gmail.com>
2023-08-01 14:28:18 +0900

    Fix: 시간 위치, 주차 종류 수정

--- front/src/page/Header.js
+++ front/src/page/Header.js
@@ -1,18 +1,10 @@
-import React, { useState, useEffect } from "react"; // React를 import해야 합니다.
 import "../style/Header.css";
 
 const Header = () => {
-  const [currentTime, setCurrentTime] = useState(new Date());
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setCurrentTime(new Date());
-    }, 1000);
-    return () => clearInterval(timer);
-  }, []);
   return (
     <>
       <header>
-        브라이튼 스마트주차유도 서비스 {currentTime.toLocaleTimeString()}
+        실시간 스마트주차 유도시스템
       </header>
     </>
   );
--- front/src/page/Left.js
+++ front/src/page/Left.js
@@ -1,69 +1,78 @@
 import React, { useEffect, useState } from "react";
 import "../style/Left.css";
 import { FiRefreshCcw } from "react-icons/fi";
-import RTSP from "./RTSP";
-import CCTVPlayer from "../Rtsp";
 import {useNavigate} from "react-router-dom"
 
 const Left = (props) => {
-  const { avail, total } = props;
-  const [done, setDone] = useState(0);
-  const [modalOpen, setModalOpen] = useState(false);
+  const {avail, total} = props;
+  const [currentDateTime, setCurrentDateTime] = useState(new Date());
 
   const navigate = useNavigate();
 
-  const Full = {
-    backgroundColor: avail === 0 ? "red" : "#6d6d6d",
-    borderRadius: '5px',
-    margin: '5px',
-    marginTop: '15px',
-    height: '65px',
-    fontSize: '30px',
-    display: 'flex',
-    alignItems: 'center',
-    paddingLeft: '10px',
-    paddingRight: '10px',
-    width: '190px',
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentDateTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []); 
+
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    weekday: 'long',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false,
   };
-
-  const Avail = {
-    backgroundColor: avail === 0 ? "#6d6d6d" : "blue",
-  }
-
+  
   const Refresh = () => {
     window.location.reload();
-  };
-
-  const showRTSP = () => {
-    // rtsp cctv영상 modal 창 띄우기
-    setModalOpen(true);
   };
 
   const RTSP = () => {
     navigate('/cctv')
   }
 
-  useEffect(() => {
-    setDone(total - avail);
-  }, [avail, total]);
-
   return (
     <>
       <aside>
         <button onClick={Refresh}><FiRefreshCcw/></button>
-        <p style={Full}>만차</p>
-        <div className="textnum" style={Avail} >
-            <p >주차가능</p> 
-            <p>{avail}</p>
-        </div>
-        <div className="textnum">
-            <p>주차완료</p> 
-            <p>{done}</p>
-        </div>
-        <div className="textnum">
-            <p>총 주차</p> 
-            <p>{total}</p>
-        </div>
+        <table border={1}>
+          <thead>
+            <tr style={{backgroundColor: 'blue'}}>
+              <th colspan="2">{currentDateTime.toLocaleString('KO-kr', options)}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr> 
+              <td>주차가능대수</td>
+              <td>{avail}/{total}</td>
+            </tr>
+            <tr>
+              <td>전기차</td>
+              <td>25</td>
+            </tr>
+            <tr>
+              <td>장애인</td>
+              <td>25</td>
+            </tr>
+            <tr>
+              <td>여성</td>
+              <td>25</td>
+            </tr>
+            <tr>
+              <td>경차</td>
+              <td>25</td>
+            </tr>
+            <tr>
+              <td>일반</td>
+              <td>25</td>
+            </tr>
+          </tbody>
+        </table>
         <button className="cctvbtn" onClick={RTSP}>CCTV영상확인</button>
       </aside>
     </>
