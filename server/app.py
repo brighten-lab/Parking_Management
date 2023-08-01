2d77e9318a9181690677f8b1ab658ea9820232c7
heymin2 <alddlv81@gmail.com>
2023-08-01 16:02:28 +0900

    Fix: 중복 제거 후에 맞게 쿼리문 수정

--- server/app.py
+++ server/app.py
@@ -28,7 +28,14 @@ def avail():
     zone = request.json.get('zone') 
     db = pymysql.connect(host='211.57.200.6', port=3306, user='root', password='willcam1190', db='park', charset='utf8')
     cursor = db.cursor()
-    sql = "SELECT count(id) FROM parking WHERE zone= %s AND is_parked= 0;"
+    sql = f"""SELECT COUNT(*)
+            FROM parking
+            WHERE id IN(
+            SELECT MAX(id) AS last_idx
+            FROM parking
+            WHERE ZONE=%s AND is_parked = 0
+            group BY spot_number)
+            ORDER BY spot_number ASC"""
     cursor.execute(sql, zone)
     result = cursor.fetchone()[0]
     db.close()
@@ -40,7 +47,14 @@ def type():
     type = request.json.get('type')
     db = pymysql.connect(host='211.57.200.6', port=3306, user='root', password='willcam1190', db='park', charset='utf8')
     cursor = db.cursor()
-    sql = "SELECT count(id) FROM parking WHERE zone= %s AND is_parked= 0 AND type=%s;"
+    sql = f"""SELECT COUNT(*)
+            FROM parking
+            WHERE id IN(
+            SELECT MAX(id) AS last_idx
+            FROM parking
+            WHERE ZONE=%s AND is_parked = 0 AND type=%s
+            group BY spot_number)
+            ORDER BY spot_number ASC"""
     cursor.execute(sql, (zone, type))
     result = cursor.fetchone()[0]
     db.close()
