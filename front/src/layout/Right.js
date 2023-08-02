9a4afc3e08991a01f9f6761c1bfa77107eeee8a9
heymin2 <alddlv81@gmail.com>
2023-08-02 11:37:25 +0900

    Feat: 버튼 스타일 추가

--- front/src/layout/Right.js
+++ front/src/layout/Right.js
@@ -1,4 +1,15 @@
 import { FiRefreshCcw } from "react-icons/fi";
+import styled from "styled-components";
+
+const Button = styled.div`
+  background-color: #4b4949;
+  color: white;
+  display: flex; 
+  margin-top: 20px;
+  margin-left: 5px;
+  font-size: 30px;
+  border:none;
+`;
 
 const Right = () => {
     const Refresh = () => {
@@ -8,7 +19,7 @@ const Right = () => {
   return (
     <>
      <aside>
-     <button onClick={Refresh}><FiRefreshCcw/></button> 
+     <Button onClick={Refresh}><FiRefreshCcw/></Button> 
      </aside>
     </>
   );
